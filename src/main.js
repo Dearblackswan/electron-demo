676ea66c00418ef351670bf77cd541327b6e7e25
dearblackswan <drl8@me.com>
2018-06-13 16:56:48 +0200

    finish

--- src/main.js
+++ src/main.js
@@ -1,7 +1,36 @@
 const electron = require('electron');
 
 const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const Menu = electron.Menu
+
+let mainWindow
 
 app.on('ready', _ => { 
-    console.log('ready!')
+    //console.log('ready!')
+    mainWindow = new BrowserWindow ()
+
+    const name = electron.app.getName()
+    const template = [
+        {
+            //label: electron.app.getName(),
+            label: name,
+            submenu: [{
+                label: `About ${name}`,
+                click: _ => {
+
+                },
+                role: 'about'
+            },{
+                type: 'separator'
+            },{
+                label: 'Quit',
+                click: _ => { app.quit() },
+                accelerator: 'Ctrl+Q'
+            }]
+        }
+    ]
+
+    const menu = Menu.buildFromTemplate(template)
+    Menu.setApplicationMenu(menu)
 })
