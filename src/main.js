eaa24465bc0333240be485f25a4fc0f0e4f9e217
dearblackswan <drl8@me.com>
2018-06-13 18:16:41 +0200

    final

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
 const electron = require('electron')
 
-const { app, Tray } = electron
+const { app, Menu, Tray } = electron
 
 const path = require('path')
 
  app.on('ready', _ => { 
     const tray = new Tray(path.join('src', 'icon.png'))
-    const contextMenu = Menu.buildFromTemplate(þ[
+    const contextMenu = Menu.buildFromTemplate([
         { 
             label: 'Wow',
             click: _ => console.log('wow')
         }
     ])
     tray.setContextMenu(contextMenu)
+    tray.setToolTip('My great app')
 }) 
