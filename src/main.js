80ea877db45e756d23d8fb16b4049df285f394b1
dearblackswan <drl8@me.com>
2018-06-13 18:33:30 +0200

    initial

--- src/main.js
+++ src/main.js
@@ -1,7 +1,30 @@
-const electron = require('electron');
+const electron = require('electron')
+const path = require('path')
 
-const app = electron.app
+const {app, clipboard, Menu, Tray} = electron
+
+function addToStack(item, stack){
+    return [item].concat(stack.length >)
+}
+
+function checkClipboardForChange(clipboard){
+    let cache = clipboard.readText()
+    let latest
+    setInterval(_ => {
+        latest = clipboard.readText()
+        if (latest !== cache) {
+            cache = latest
+            onchange(cache)
+        }
+    }, 1000)
+}
 
 app.on('ready', _ => { 
-    console.log('ready!')
+    let stack = []
+    const tray = new Tray(path.join('src', 'eye.png'))
+    tray.setContextMenu(Menu.buildFromTemplate([{label: '<Empty>', enabled: false}]))
+
+    checkClipboardForChange(clipboard, text => {
+        addToStack(text, stack)
+    } )
 })
