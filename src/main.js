663bd3cedf47635c43f5c6b66b2d9aff730d89f2
dearblackswan <drl8@me.com>
2018-06-13 15:25:44 +0200

    finish

--- src/main.js
+++ src/main.js
@@ -1,7 +1,30 @@
 const electron = require('electron');
 
-const app = electron.app
+const countdown = require('./countdown')
+
+const {app, BrowserWindow} = require('electron')
+
+const ipc = electron.ipcMain
+
+let mainWindow
 
 app.on('ready', _ => { 
-    console.log('ready!')
+    //console.log('ready!')
+    mainWindow = new BrowserWindow ({
+        height: 600,
+        width: 600
+    })
+
+    mainWindow.loadURL(`file://${__dirname}/countdown.html`)
+
+    mainWindow.on('closed', _ => {
+        //console.log('closed !')
+        mainWindow = null
+    })
+})
+
+ipc.on('countdown-start', _ => {
+    countdown(count => {
+        mainWindow.webContents.send('countdown', count)
+    })
 })
