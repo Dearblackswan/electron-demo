2723a0b2fa3f535293fed6f5de1711ced40f3520
dearblackswan <drl8@me.com>
2018-06-18 16:53:55 +0200

    final

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 const electron = require('electron');
 
-const app = electron.app
+const { app, BrowserWindow } = electron
+
+let mainWindow
 
 app.on('ready', _ => { 
-    console.log('ready!')
+    mainWindow = new BrowserWindow ({
+        width: 400,
+        height: 100
+    })
+
+    mainWindow.loadURL(`file://${__dirname}/status.html`)
+
+    mainWindow.on('close', _ => {
+        mainWindow = null
+    })
 })
